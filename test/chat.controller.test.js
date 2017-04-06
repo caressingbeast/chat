a17f400f68e66313281c94413551eb1a43bd0211
Brandon Burning <brandonb@ecreations.net>
2017-04-06 11:08:44 -0700

    More tests

--- test/chat.controller.test.js
+++ test/chat.controller.test.js
@@ -2,21 +2,28 @@ describe('ChatCtrl', function () {
 
   beforeEach(module('chat'));
 
+  var refreshEvent = 'refreshEvent';
+
   // placeholders
   var $controller;
+  var $http;
   var $scope;
 
-  // mocks
+  // mock SocketService
   var SocketService = {
-    on: function (event, callback) {},
-    emit: function (event, data, callback) {}
+    on: function (event, callback) {
+      this[event] = callback;
+    },
+    emit: function (event, data, callback) {
+      if (this[event]) {
+        this[event](data);
+      }
+    }
   };
 
-  beforeEach(inject(function(_$controller_, $rootScope) {
+  beforeEach(inject(function(_$controller_, $httpBackend, $rootScope) {
+    $http = $httpBackend;
     $scope = $rootScope.$new();
-
-    spyOn(SocketService, 'on').and.callThrough();
-    spyOn(SocketService, 'emit').and.callThrough();
 
     $controller = _$controller_('ChatCtrl', {
       $scope: $scope,
@@ -49,12 +56,16 @@ describe('ChatCtrl', function () {
   describe('.saveUsername()', function () {
 
     it('should do nothing if empty username', function () {
+      spyOn(SocketService, 'emit');
+
       $controller.saveUsername();
       expect(SocketService.emit).not.toHaveBeenCalled();
       expect($controller.username).toEqual('');
     });
 
     it('should do nothing if duplicate username', function () {
+      spyOn(SocketService, 'emit');
+
       $controller.users = ['duplicate'];
       $controller.formData.username = 'duplicate';
 
@@ -66,6 +77,7 @@ describe('ChatCtrl', function () {
     it('should save username and call SocketService.emit if valid username', function () {
       var username = 'username';
 
+      spyOn(SocketService, 'emit');
       $controller.formData.username = username;
 
       $controller.saveUsername();
@@ -81,6 +93,7 @@ describe('ChatCtrl', function () {
   describe('.sendMessage()', function () {
 
     it('should do nothing if empty message', function () {
+      spyOn(SocketService, 'emit');
       $controller.sendMessage();
       expect(SocketService.emit).not.toHaveBeenCalled();
     });
@@ -92,11 +105,86 @@ describe('ChatCtrl', function () {
         created_at: new Date()
       };
 
+      spyOn(SocketService, 'emit');
+
       $controller.formData.message = message.text;
       $controller.username = message.user;
 
       $controller.sendMessage();
       expect(SocketService.emit).toHaveBeenCalledWith('sendMessage', message);
+    });
+  });
+
+  describe('SocketService listeners', function () {
+
+    it('listens for "loadData"', function () {
+      var data = {
+        messages: [
+          {
+            text: 'text',
+            user: 'user',
+            created_at: new Date()
+          }
+        ],
+        users: ['user']
+      };
+
+      SocketService.emit('loadData', data);
+
+      expect($controller.messages).toEqual(data.messages);
+      expect($controller.users).toEqual(data.users);
+    });
+
+    it('listens for "pushUser"', function () {
+      var data = {
+        message: [
+          {
+            isServer: true,
+            text: 'user has joined.'
+          }
+        ],
+        username: 'user'
+      };
+
+      SocketService.emit('pushUser', data);
+
+      expect($controller.messages.length).toEqual(1);
+      expect($controller.messages[0]).toEqual(data.message);
+      expect($controller.users.length).toEqual(1);
+      expect($controller.users[0]).toEqual(data.username);
+    });
+
+    it('listens for "pushMessage"', function () {
+      var message = {
+        text: 'text',
+        user: 'user',
+        created_at: new Date()
+      };
+
+      SocketService.emit('pushMessage', message);
+      SocketService.emit('pushMessage', message);
+
+      expect($controller.messages.length).toEqual(2);
+      expect($controller.messages[0]).toEqual(message);
+    });
+
+    it('listens for "pullUser"', function () {
+      var username = 'frank';
+      var message = {
+        isServer: true,
+        text: username + ' has left.'
+      };
+
+      $controller.users = ['brandon', username];
+
+      SocketService.emit('pullUser', {
+        message: message,
+        username: username
+      });
+
+      expect($controller.messages.length).toEqual(1);
+      expect($controller.users.length).toEqual(1);
+      expect($controller.users.indexOf(username)).toEqual(-1);
     });
   });
 });
